4fea7f5acd1fc8921da7cd97f09b0e54f679c880
wa765 <wahyusaputra12345678@gmail.com>
2022-05-28 20:26:07 +0700

    add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// ========================  Color Docs  ==============================
+
+// Color Primary
+Color kPrimaryColor = const Color(0xFF2C557D);
+
+// Color Black
+Color kBlackColor = const Color(0xFF0D0D26);
+
+// Color Gray
+Color kGrayColor = const Color(0xFF95969D);
+
+// Color Light Gray
+Color kLightGrayColor = const Color(0xFFE4E5E7);
+
+// Color Background
+Color kBackgroundColor = const Color(0xFFFAFAFD);
+
+// Color Cirlce Top Background
+Color kCircleTopColor = const Color(0xFF224261);
+
+// Color White
+Color kWhiteColor = Colors.white;
+
+// ========================  Text Style Docs ==============================
+
+// Text Black
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+// Text Gray
+TextStyle grayTextStyle = GoogleFonts.poppins(color: kGrayColor);
+
+// Text Primary
+TextStyle primaryTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+
+// ========================  Font Weight Docs  ==============================
+
+FontWeight light = FontWeight.w300;
+
+FontWeight regular = FontWeight.w400;
+
+FontWeight medium = FontWeight.w500;
+
+FontWeight semiBold = FontWeight.w600;
+
+FontWeight extraBold = FontWeight.w800;
+
+FontWeight black = FontWeight.w900;
